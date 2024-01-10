0de81b47b0a19614619b741b99daa9fe6c4ba3ed
YaritzaHuizaG25 <68889188+YaritzaHuizaG25@users.noreply.github.com>
2024-01-09 21:26:05 -0600

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
+"titulo" => "Periodista ecuatoriano dice que granada usada por delincuentes en ",
 "autor" => "XXX",
 "resumen" => "XXX",
 ];
