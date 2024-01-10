277ac2f00a2b940968daef4d1540b235f9cf1073
YaritzaHuizaG25 <68889188+YaritzaHuizaG25@users.noreply.github.com>
2024-01-09 21:38:30 -0600

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
+"titulo" => "Universitario vs. César Vallejo en el Mansiche, por la 'Noche Poeta'",
 "autor" => "XXX",
-"resumen" => "XXX",
+"resumen" => "Este martes, Universitario vs. César Vallejo se ven las caras esta noche por la Noche Poeta con transmisión de Liga 1 MAX. Sigue las incidencias del duelo por Depor.",
 ];
 ?>
