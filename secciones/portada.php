76424abb4e2f84dab0263f5813fc9b78ac426322
YaritzaHuizaG25 <68889188+YaritzaHuizaG25@users.noreply.github.com>
2024-01-09 21:28:23 -0600

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "REVISTA UNS 2024",
-"autor" => "uns",
-"resumen" => "Universidad Nacional del Santa",
+"titulo" => "NOTICIAS DEL DÍA",
+"autor" => "NACIONAL E INTERNACIONAL",
+"resumen" => "IMPACTANTES",
 ];
 ?>
